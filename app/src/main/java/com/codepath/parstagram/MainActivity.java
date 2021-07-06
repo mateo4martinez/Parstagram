4d7441aa51c7ff62b951abcf34231fa128d76849
Mateo Martinez <mateom@fb.com>
2021-07-06 14:35:28 -0700

    user can logout

--- app/src/main/java/com/codepath/parstagram/MainActivity.java
+++ app/src/main/java/com/codepath/parstagram/MainActivity.java
@@ -2,13 +2,36 @@ package com.codepath.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                goLoginActivity();
+            }
+        });
+    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
